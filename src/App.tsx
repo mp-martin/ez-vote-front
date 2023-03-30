2ade2c98fa6ace20543bad9cf30e5994fa297047
mp-martin <martin.paper@gmail.com>
2023-03-30 08:37:50 +0200

    Basic file structure for design and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {AddPoll} from './components/AddPoll/AddPoll';
 import {PollToFill} from './components/PollToFill/PollToFill';
 import {PollResults} from './components/PollResults/PollResults';
+import {Header} from './components/Layout/Header/Header';
+import {Footer} from './components/Layout/Footer/Footer';
 
 const App = () => (<>
 	<AddPoll/>
