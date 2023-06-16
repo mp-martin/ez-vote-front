a9e3a5849770c0c9065a314ba1ad0eaa24a43f84
mp-martin <martin.paper@gmail.com>
2023-06-16 19:49:51 +0200

    create auth hook

--- src/hooks/use.user.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext, useState } from 'react'
-import { UserContext } from '../contexts/user.context'
-
-export const useUser = () => {
-  const { user, setUser } = useContext(UserContext)
-}
