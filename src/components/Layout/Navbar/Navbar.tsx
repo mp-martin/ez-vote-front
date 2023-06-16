ad3d4073b41c99f7e4e5b6ea8be7a0ebb30c5815
mp-martin <martin.paper@gmail.com>
2023-06-16 19:53:21 +0200

    navbar adjustments

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import './Navbar.css';
+import React from 'react'
+import './Navbar.css'
+import { useAuth } from '../../../hooks/use.auth'
 
-export const Navbar = () => (<div className='Navbar'>
-	<a href='#' style={{textDecoration: 'line-through', opacity: 0.4}}>Login</a>
-	<a href='#' style={{color: 'white', fontWeight: 700, textDecoration: 'line-through', opacity: 0.4}}>Sign up</a>
-	<a href='#about'>About</a>
-</div>);
+export const Navbar = () => {
+  const { user } = useAuth()
+
+  return (
+        <>
+            {(user != null) ? `ohaio yusaaa ${user.userLogin}` : 'mistaaa ju not loginnn'}
+            <div className='Navbar'>
+                <a href='/login'>Login</a>
+                <a href='#' style={{ color: 'white', fontWeight: 700, textDecoration: 'line-through', opacity: 0.4 }}>Sign
+                    up</a>
+                <a href='#about'>About</a>
+            </div>
+        </>
+  )
+}
