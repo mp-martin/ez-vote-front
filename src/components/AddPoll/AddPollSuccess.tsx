1de7ac0c3600b859a252b11673c6061abceaa167
mp-martin <martin.paper@gmail.com>
2023-06-18 15:06:56 +0200

    refactor

--- src/components/AddPoll/AddPollSuccess.tsx
+++ src/components/AddPoll/AddPollSuccess.tsx
@@ -20,9 +20,11 @@ export const AddPollSuccess = (props: Props) => {
             <div className='addPoll__final-link'><p>Share this link with others so they can start voting:</p>
                 <div className='addPoll__final-link_address'>
                     <a href={`http://${location}:3000/poll/${props.id}`}>{`http://${location}:3000/poll/${props.id}`}</a>
-                    <div className='addPoll__copy-to-clipboard'><p>Copy to clipboard:</p> <FaCopy size={'2em'}
-                                                                                                  className='addPoll__copy-button'
-                                                                                                  onClick={async () => await copy(`http://${location}:3000/poll/${props.id}`)}/>
+                    <div className='addPoll__copy-to-clipboard'><p>Copy to clipboard:</p>
+                        <FaCopy
+                            size={'2em'}
+                            className='addPoll__copy-button'
+                            onClick={async () => await copy(`http://${location}:3000/poll/${props.id}`)}/>
                     </div>
                 </div>
             </div>
@@ -30,9 +32,10 @@ export const AddPollSuccess = (props: Props) => {
             <div className='addPoll__final-link'><p>To see the results, go to this link:</p>
                 <div className='addPoll__final-link_address'>
                     <a href={`http://${location}:3000/poll/${props.id}/results`}>{`http://${location}:3000/poll/${props.id}/results`}</a>
-                    <div className='addPoll__copy-to-clipboard'><p>Copy to clipboard:</p> <FaCopy size={'2em'}
-                                                                                                  className='addPoll__copy-button'
-                                                                                                  onClick={async () => await copy(`http://${location}:3000/poll/${props.id}/results`)}/>
+                    <div className='addPoll__copy-to-clipboard'><p>Copy to clipboard:</p>
+                        <FaCopy size={'2em'}
+                                className='addPoll__copy-button'
+                                onClick={async () => await copy(`http://${location}:3000/poll/${props.id}/results`)}/>
                     </div>
                 </div>
             </div>
