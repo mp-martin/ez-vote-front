ef007b930a11131fcf0909cdcf015dc9fc935f71
mp-martin <martin.paper@gmail.com>
2023-06-18 13:31:00 +0200

    isolated message context to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,19 @@ import { Main } from './components/Layout/Main/Main'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Message } from './components/common/Message/Message'
-import { MessageContext } from './contexts/message.context'
+import { MessageContext, MessageContextProvider } from './contexts/message.context'
 import { UserContextProvider } from './contexts/user.context'
 import { Login } from './components/Login/Login'
 import { Dashboard } from './components/Dashboard/Dashboard'
 import { Register } from './components/Register/Register'
 
 const App = (): JSX.Element => {
-  const [showMessage, setShowMessage] = useState(false)
-  const [messageContent, setMessageContent] = useState('')
-  const [messageTimer, setMessageTimer] = useState(0)
-  const [messageType, setMessageType] = useState('')
-
   return (
         <div className='App'>
             <UserContextProvider>
-                <MessageContext.Provider value={{ showMessage, setShowMessage, setMessageContent, setMessageTimer, setMessageType }}>
-                    <Dashboard><Header/>
+                <MessageContextProvider>
+                    <Dashboard>
+                        <Header/>
                         <Routes>
                             <Route path='/' element={<Main/>}/>
                             <Route path='/addpoll' element={<AddPoll/>}/>
@@ -35,10 +31,8 @@ const App = (): JSX.Element => {
                             <Route path='/:whatever' element={<Main/>}/>
                         </Routes>
                         <Footer/>
-                        {showMessage && <Message content={messageContent} type={messageType} timer={messageTimer} onClose={() => {
-                          setShowMessage(false)
-                        }}/>}</Dashboard>
-                </MessageContext.Provider>
+                    </Dashboard>
+                </MessageContextProvider>
             </UserContextProvider>
         </div>
   )
--- src/contexts/message.context.tsx
+++ src/contexts/message.context.tsx
@@ -1,13 +1,19 @@
-import { createContext, type Dispatch, type SetStateAction } from 'react'
+import React, { createContext, type Dispatch, type PropsWithChildren, type SetStateAction, useState } from 'react'
 
 export const MessageContext = createContext<{
   showMessage: boolean
+  messageTimer: number
+  messageType: string
+  messageContent: string
   setShowMessage: Dispatch<SetStateAction<boolean>>
   setMessageContent: Dispatch<SetStateAction<string>>
   setMessageTimer: Dispatch<SetStateAction<number>>
   setMessageType: Dispatch<SetStateAction<string>>
 }>(
   {
+    messageContent: '',
+    messageTimer: 0,
+    messageType: '',
     showMessage: false,
     setShowMessage: () => {
     },
@@ -18,3 +24,24 @@ export const MessageContext = createContext<{
     setMessageType: () => {
     }
   })
+
+export const MessageContextProvider = (props: PropsWithChildren) => {
+  const [showMessage, setShowMessage] = useState(false)
+  const [messageContent, setMessageContent] = useState('')
+  const [messageTimer, setMessageTimer] = useState(0)
+  const [messageType, setMessageType] = useState('')
+  return (
+        <MessageContext.Provider
+            value={{
+              showMessage,
+              messageType,
+              messageTimer,
+              messageContent,
+              setShowMessage,
+              setMessageContent,
+              setMessageTimer,
+              setMessageType
+            }}>
+            {props.children}
+        </MessageContext.Provider>)
+}
